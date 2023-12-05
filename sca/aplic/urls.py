efbe77d5401c9765244ae2d54cab974491e6198a
wechiley <alveswechiley@gmail.com>
2023-12-05 00:58:58 -0300

    ajuste na urls, view

--- sca/aplic/urls.py
+++ sca/aplic/urls.py
@@ -1,12 +1,16 @@
 from django.conf import settings
 from django.urls import path
-from .views import IndexView, PecaView
+from .views import IndexView, PecaView, SearchPecaView
+
 from django.conf.urls.static import static
+ 
+
 
 urlpatterns = [
     path('', IndexView.as_view(), name="index"),
-    path('pecas.html', PecaView.as_view(), name ='pecas')
-       
+    path('pecas.html', PecaView.as_view(), name ='pecas'),
+    path('search_pecas', SearchPecaView.as_view(), name='search_pecas'),
+    
 ]
 
 if settings.DEBUG:
--- sca/aplic/views.py
+++ sca/aplic/views.py
@@ -1,11 +1,14 @@
+from django.db.models import Q
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from aplic.models import Peca
 from .models import Peca
 
 def index(request):
     return render(request, 'index.html')
+
 class IndexView(TemplateView):
-    template_name = 'index.html'
+    template_name = 'index.html'   
 
 class PecaView(TemplateView):
     template_name = 'pecas.html'
@@ -14,3 +17,26 @@ class PecaView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['pecas'] = Peca.objects.all()
         return context
+    
+    
+class SearchPecaView(TemplateView):
+    template_name = 'pecas.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        query = self.request.GET.get('q')
+        print("Query:", query)
+        context['pecas'] = Peca.objects.filter(TipoPeca__icontains=query)
+        return context
+    
+
+
+        
+
+    
+    
+
+    
+    
+
+
